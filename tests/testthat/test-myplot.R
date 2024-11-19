97fac628601c507f6706b47942ee40709a2db6f3
Lingyi <fly.karrie@gmail.com>
2024-11-19 12:42:59 -0700

    add vignettes

--- tests/testthat/test-myplot.R
+++ tests/testthat/test-myplot.R
@@ -1,6 +1,6 @@
 test_that("multiplication works", {
   expect_error(myplot(1, 'o'))
   expect_error(myplot('a', 1))
-  expect_s3_class(myplot(1,1), 'list')
-
+  result <- myplot(1, 1)
+  expect_s3_class(result, 'list')
 })
